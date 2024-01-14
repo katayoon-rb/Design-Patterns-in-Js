969f08f60b27c1d4a22dff08768dbec0875b6f92
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-14 23:28:38 +0330

    Add files via upload

---
+++ 17 - Iterator Pattern/01 Array Backed Props.js
@@ -0,0 +1,34 @@
+class Creature {
+  constructor() {
+    this.stats = new Array(3).fill(10)
+  }
+
+  get strength() { return this.stats[0] }
+  set strength(value) { this.stats[0] = value }
+
+  get agility() { return this.stats[1] }
+  set agility(value) { this.stats[1] = value }
+
+  get intelligence() { return this.stats[2] }
+  set intelligence(value) { this.stats[2] = value }
+
+  get sumOfStats() {
+    return this.stats.reduce((x, y) => x + y, 0)
+  }
+  get averageStat() {
+    return this.sumOfStats / this.stats.length
+  }
+  get maxStat() {
+    return Math.max(...this.stats)
+  }
+}
+
+let creature = new Creature()
+creature.strength = 10
+creature.agility = 11
+creature.intelligence = 15
+console.log(
+  `Creature has average stat ${creature.averageStat}, `
+  + `max stat = ${creature.maxStat}, `
+  + `sum of stats = ${creature.sumOfStats}.`
+)
---
+++ 17 - Iterator Pattern/02 Behavioral Iterator.js
@@ -0,0 +1,43 @@
+class Stuff {
+  constructor() {
+    this.a = 11
+    this.b = 22
+  }
+
+  [Symbol.iterator]() {
+    let i = 0
+    let self = this
+    return {
+      next: () => {
+        return {
+          done: i > 1,
+          value: self[i++ === 0 ? 'a' : 'b']
+        }
+      }
+    }
+  }
+
+  get backwards() {
+    let i = 0
+    let self = this
+    return {
+      next: () => {
+        return {
+          done: i > 1,
+          value: self[i++ === 0 ? 'b' : 'a']
+        }
+      },
+      [Symbol.iterator]: function() { return this }
+    }
+  }
+}
+
+let values = [100, 200, 300]
+for (let i in values) {
+  console.log(`Element at pos ${i} is ${values[i]}`)
+}
+for (let v of values) { console.log(`Value is ${v}`) }
+
+let stuff = new Stuff()
+for (let item of stuff) { console.log(`${item}`) }
+for (let item of stuff.backwards) { console.log(`${item}`) }
